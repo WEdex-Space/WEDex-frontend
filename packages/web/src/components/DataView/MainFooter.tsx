db0f6dddf91df0823102a08770d677cc106e4bef
refined-x <tower1229@foxmail.com>
2022-12-17 17:11:16 +0800

    feat(web): ✨ add Dataview list

--- packages/web/src/components/DataView/MainFooter.tsx
+++ packages/web/src/components/DataView/MainFooter.tsx
@@ -1,114 +1,89 @@
+import { USelect } from '@wedex/components'
+import { CustomOutlined, ExportOutlined } from '@wedex/icons'
 import { defineComponent, ref } from 'vue'
+
+const footerCellClass =
+  'border-l-1 border-color-border 2xl:px-7 px-3 leading-10 cursor-pointer hover:text-color2'
+const footerCellIconClass = 'w-4 h-4 align-middle mx-1 -mt-[2px]'
+const footerListCellClass = '2xl:px-7 px-3 border-r-1 border-color-border cursor-default'
 
 export default defineComponent({
   name: 'MainFooter',
   setup() {
-    const typeData = ref([
+    const formData = ref({
+      range: 5
+    })
+
+    const rangeData = ref([
       {
-        name: 'Hot pairs',
+        label: 'Last 5 mins',
         value: 1
       },
       {
-        name: 'All pairs',
+        label: 'Last 1 hour',
         value: 2
       },
       {
-        name: 'New pairs',
+        label: 'Last 4 hours',
         value: 3
       },
       {
-        name: 'Trends',
+        label: 'Last 6 hours',
         value: 4
       },
       {
-        name: 'Gainers & Losers',
+        label: 'Last 24 hours',
         value: 5
-      },
-      {
-        name: 'Ranking',
-        value: 6
-      },
-      {
-        name: 'Launch',
-        value: null,
-        link: 'https://weconomy.space'
       }
     ])
-
-    const DEXesData = ref([
-      {
-        id: 1,
-        name: 'DEXes1'
-      },
-      {
-        id: 2,
-        name: 'DEXes2'
-      },
-      {
-        id: 3,
-        name: 'DEXes3'
-      },
-      {
-        id: 4,
-        name: 'DEXes4'
-      },
-      {
-        id: 5,
-        name: 'DEXes5'
-      }
-    ])
-
-    const tagData = ref([
-      {
-        name: 'All',
-        value: null
-      },
-      {
-        name: 'Defi',
-        value: 'Defi'
-      },
-      {
-        name: 'DEX',
-        value: 'DEX'
-      },
-      {
-        name: 'NFT',
-        value: 'NFT'
-      },
-      {
-        name: 'Gaming',
-        value: 'Gaming'
-      },
-      {
-        name: 'Layer-1',
-        value: 'Layer-1'
-      },
-      {
-        name: 'Metaverse',
-        value: 'Metaverse'
-      }
-    ])
-
-    const formData = ref<{
-      chainId: number | null
-      type: number | null
-      DEXe: number | null
-      tag: string | null
-    }>({
-      chainId: null,
-      type: 2,
-      DEXe: null,
-      tag: null
-    })
 
     return {
-      typeData,
-      DEXesData,
-      tagData,
-      formData
+      formData,
+      rangeData
     }
   },
   render() {
-    return <div class="border-color-border border-t-1 h-10">foot filter</div>
+    return (
+      <div class="border-color-border flex border-t-1 h-10 text-color3 ">
+        <ul class="flex flex-1 items-center">
+          <li class={footerListCellClass}>
+            Tokens:<span class="text-color1">1234</span>
+          </li>
+          <li class={footerListCellClass}>
+            Networks:<span class="text-color1">1234</span>
+          </li>
+          <li class={footerListCellClass}>
+            DEXes:<span class="text-color1">1234</span>
+          </li>
+          <li class={footerListCellClass}>
+            Pools:<span class="text-color1">1234</span>
+          </li>
+          <li class={footerListCellClass}>
+            24h Txns:<span class="text-color1">1234</span>
+          </li>
+          <li class={footerListCellClass}>
+            24h Volumn:<span class="text-color1">1234</span>
+          </li>
+        </ul>
+        <div class={`border-l-1 border-color-border w-40`}>
+          <USelect
+            class={`selectTransparent`}
+            placeholder="Networks"
+            clearable
+            v-model:value={this.formData.range}
+            options={this.rangeData}
+            onUpdate:value={value => (this.formData.range = value)}
+          ></USelect>
+        </div>
+        <div class={footerCellClass}>
+          <CustomOutlined class={footerCellIconClass} />
+          Customize
+        </div>
+        <div class={footerCellClass}>
+          <ExportOutlined class={footerCellIconClass} />
+          Export
+        </div>
+      </div>
+    )
   }
 })
